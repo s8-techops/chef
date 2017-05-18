b70580d94c75405e02e20c1e852b66e24f90f3bb
George Lei <glei@storm8.com>
2017-05-18 16:14:05 -0700

    update setup.sh path

--- cookbooks/test_file/recipes/default.rb
+++ cookbooks/test_file/recipes/default.rb
@@ -4,8 +4,15 @@ file '/root/test' do
   mode "0644"
 end
 
+file '/root/setup.sh' do
+  content IO.read('/root/chef/vagrant/scripts/setup.sh')
+  mode "0755"
+  action :create
+end
+
+
 cron 'setup' do
   minute '*/30'
-  command '/vagrant/scripts/setup.sh > /tmp/setup.log 2>&1'
+  command '/root/setup.sh > /tmp/setup.log 2>&1'
   action :create
 end
